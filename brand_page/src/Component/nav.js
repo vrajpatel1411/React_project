96b439d0e7c4d3609bf12bff17bd4fa4b6cb5ca7
Vraj Patel <vraj.patel.12390@gmail.com>
2023-08-16 00:37:51 -0400

    Css updated

--- brand_page/src/Component/nav.js
+++ brand_page/src/Component/nav.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import '../Styles/nav.css'
+
+
+                    
+                 
+const hamburgerClick=()=>{
+    const hamburger=document.querySelector(".hamburger");
+    hamburger.classList.toggle("active");
+    const navmenu=document.querySelector("#nav-menu");
+    navmenu.classList.toggle("active");
+}
+
+const handleScroll=()=>{
+    const navmenu=document.querySelector("#nav-menu");
+    navmenu.style.left="-100px";
+}
 function Nav(){
     return(
-        <nav>
+        <nav onScroll={handleScroll}>
+            
             <div className="logo">
-                <img src="/Images/brand_logo.png" alt=""/>
-      
+                <img src="/Images/brand_logo.png" alt="Not Found"/>
             </div>
-            <ul>
+            <ul id="nav-menu">
                 <li href="#">Menu</li>
                 <li href="#">Location</li>
                 <li href="#">About</li>
                 <li href="#">Contact</li>
             </ul>
             <button>Login</button>
+
+            <div class="hamburger" onClick={hamburgerClick}>
+                <span class="bar"></span>
+                
+                <span class="bar"></span>
+                
+                <span class="bar"></span>
+            </div>
+            
         </nav>
     );
 
